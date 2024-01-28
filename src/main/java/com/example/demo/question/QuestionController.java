ebe576c07c02ec35a26c004d82c9dcf7a7ecf330
hesuhesu <zndwk0728@gmail.com>
2024-01-29 00:24:47 +0900

    update 24.01.29

--- src/main/java/com/example/demo/question/QuestionController.java
+++ src/main/java/com/example/demo/question/QuestionController.java
@@ -14,7 +14,9 @@ import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 import com.example.demo.answer.AnswerForm;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.example.demo.user.SiteUser;
@@ -59,4 +61,45 @@ public class QuestionController {
 		this.questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
 		return "redirect:/question/list";
 	}
+	
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/modify/{id}")
+	public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id, Principal principal) {
+		Question question = this.questionService.getQuestion(id);
+		if(!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, " 수 정 권 한 이 없 습 니 다 .");
+		}
+		questionForm.setSubject(question.getSubject());
+		questionForm.setContent(question.getContent());
+		return "question_form";
+	}
+	
+	@PreAuthorize("isAuthenticated()")
+	@PostMapping("/modify/{id}")
+	public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal, @PathVariable("id") Integer id) {
+	
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		Question question = this.questionService.getQuestion(id);
+		if (!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, " 수 정 권 한 이 없 습 니 다 .");
+		}
+		this.questionService.modify(question, questionForm.getSubject(),
+				questionForm.getContent());
+		return String.format("redirect:/question/detail/%s", id);
+	}
+	
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/delete/{id}")
+	public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		if (!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, " 삭 제 권 한 이 없 습 니 다 .");
+		}
+		this.questionService.delete(question);
+		return "redirect:/";
+	}
+	
+	
 }
